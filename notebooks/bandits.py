d487a7feefc0d1628891e101e026c84854c7086a
Senthooran Rajamanoharan <s.rajamanoharan@gmail.com>
2022-01-13 20:30:40 +0000

    Refactored experiment so that results is a separate object that can be loaded and saved

--- notebooks/bandits.py
+++ notebooks/bandits.py
@@ -1,4 +1,7 @@
-from typing import Dict, Callable, Any, Sequence, Optional, List
+from typing import Dict, Callable, Any, Sequence, Optional, List, Union
+import os
+from pathlib import Path
+import pickle
 import numpy as np
 import pandas as pd  # type: ignore
 import matplotlib.pyplot as plt  # type: ignore
@@ -11,80 +14,37 @@ from rl.agents import EpsilonGreedyRewardAveragingAgent
 from rl.callbacks import AgentStateLogger
 
 
-class BanditLearningCurves:
-    """Bandit learning curves experiment.
+class BanditResults:
+    """Results of bandit experiment.
 
-    Calculates learning curves for the supplied agents. Stores results
-    in the `results` attribute.
-
-    Args:
-      agent_builders: a dict with agent names as keys and callables
-        that build the corresponding agents as values
-      test_bed_size: number of random bandits to generate
-      n_steps: length of simulations
-      random_state: `None`, random seed or random number generator
+    Returned by `bandit_experiment`, contains methods for analysing
+    and visualising results, as well as for loading and saving them.
     """
 
-    def __init__(
-        self,
-        agent_builders: Dict[str, Callable[[], Agent]],
-        test_bed_size: int,
-        n_steps: int,
-        random_state=None,
-    ):
-        n_levers = 10
-        bandit_mean_params, bandit_sigma_params = (0.0, 1.0), (1.0, 0.0)
-        results = []
-        for bandit_id in range(test_bed_size):
-            bandit = random_bandit(
-                n_levers,
-                mean_params=bandit_mean_params,
-                sigma_params=bandit_sigma_params,
-                random_state=random_state,
-            )
-            actions_ordered_by_value_desc = np.argsort(bandit.means)[::-1]
-            action_ranks = np.argsort(actions_ordered_by_value_desc)
-            for agent_name, agent_builder in agent_builders.items():
-                agent = agent_builder()
-                callbacks = [AgentStateLogger()]
-                sim = SingleAgentWaitingSimulator(
-                    bandit, agent, callbacks=callbacks
-                )
-                sim.run(n_steps)
-                actions_taken_ranks = action_ranks[sim.history.actions]
-                greedy_actions_ranks = action_ranks[
-                    [np.argmax(s["Q"]) for s in callbacks[0].states]
-                ]
-                results.append(
-                    {
-                        "bandit_id": bandit_id,
-                        "agent": agent_name,
-                        "ranked_action_values": bandit.means[
-                            actions_ordered_by_value_desc
-                        ],
-                        "action_ranks": actions_taken_ranks,
-                        "actions_taken_optimal": (
-                            (actions_taken_ranks == 0).astype(np.float_)
-                        ),
-                        "greedy_actions": greedy_actions_ranks,
-                        "greedy_actions_optimal": (
-                            (greedy_actions_ranks == 0).astype(np.float_)
-                        ),
-                        "rewards": np.array(sim.history.rewards),
-                    }
-                )
+    def __init__(self, results: List[Dict[str, Any]]):
         self._results = results
 
     @property
     def results(self) -> List[Dict[str, Any]]:
         return self._results
 
+    def save(self, file_path: Union[str, os.PathLike]):
+        save_path = Path(file_path)
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(save_path, "wb") as f:
+            pickle.dump(self._results, f)
+
+    @classmethod
+    def load(cls, file_path: Union[str, bytes, os.PathLike]):
+        with open(file_path, "rb") as f:
+            return cls(pickle.load(f))
+
     @property
     def average_optimal_action_value(self):
         """Mean optimal action value over the test bed."""
         return np.mean([r["ranked_action_values"][0] for r in self.results])
 
-    def aggregated_results_summary(self) -> pd.DataFrame:
+    def summary(self) -> pd.DataFrame:
         """Summarises results and returns as a DataFrame.
 
         Returned DataFrame contains the following fields:
@@ -122,10 +82,10 @@ class BanditLearningCurves:
             .reset_index()
         )
 
-    def plot_results(self) -> None:
+    def plot(self) -> None:
         """Plots aggregated results."""
         sns.set_style("whitegrid")
-        results_summary = self.aggregated_results_summary()
+        results_summary = self.summary()
         f, axs = plt.subplots(1, 3, figsize=(18, 4))
         plt.sca(axs[0])
         sns.lineplot(data=results_summary, x="t", y="mean_reward", hue="agent")
@@ -158,6 +118,70 @@ class BanditLearningCurves:
         plt.ylabel("% Optimal action identified")
         plt.legend()
         plt.show()
+
+
+def bandit_experiment(
+    agent_builders: Dict[str, Callable[[], Agent]],
+    test_bed_size: int,
+    n_steps: int,
+    random_state=None,
+) -> BanditResults:
+    """Bandit learning curves experiment.
+
+    Calculates learning curves for the supplied agents
+
+    Args:
+      agent_builders: a dict with agent names as keys and callables
+        that build the corresponding agents as values
+      test_bed_size: number of random bandits to generate
+      n_steps: length of simulations
+      random_state: `None`, random seed or random number generator
+
+    Returns:
+      `BanditResults` object containing results
+    """
+    n_levers = 10
+    bandit_mean_params, bandit_sigma_params = (0.0, 1.0), (1.0, 0.0)
+    results = []
+    for bandit_id in range(test_bed_size):
+        bandit = random_bandit(
+            n_levers,
+            mean_params=bandit_mean_params,
+            sigma_params=bandit_sigma_params,
+            random_state=random_state,
+        )
+        actions_ordered_by_value_desc = np.argsort(bandit.means)[::-1]
+        action_ranks = np.argsort(actions_ordered_by_value_desc)
+        for agent_name, agent_builder in agent_builders.items():
+            agent = agent_builder()
+            callbacks = [AgentStateLogger()]
+            sim = SingleAgentWaitingSimulator(
+                bandit, agent, callbacks=callbacks
+            )
+            sim.run(n_steps)
+            actions_taken_ranks = action_ranks[sim.history.actions]
+            greedy_actions_ranks = action_ranks[
+                [np.argmax(s["Q"]) for s in callbacks[0].states]
+            ]
+            results.append(
+                {
+                    "bandit_id": bandit_id,
+                    "agent": agent_name,
+                    "ranked_action_values": bandit.means[
+                        actions_ordered_by_value_desc
+                    ],
+                    "action_ranks": actions_taken_ranks,
+                    "actions_taken_optimal": (
+                        (actions_taken_ranks == 0).astype(np.float_)
+                    ),
+                    "greedy_actions": greedy_actions_ranks,
+                    "greedy_actions_optimal": (
+                        (greedy_actions_ranks == 0).astype(np.float_)
+                    ),
+                    "rewards": np.array(sim.history.rewards),
+                }
+            )
+    return BanditResults(results)
 
 
 def get_epsilon_greedy_bandit_agent_builder(
